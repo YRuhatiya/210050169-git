38937a2db6b50883a9dda87637e0e0553febf3e9
YRuhatiya <yashruhatiya2003@gmail.com>
2022-08-17 23:50:05 +0530

    Add polynomial rolling hash function

--- hashing.cpp
+++ hashing.cpp
@@ -2,15 +2,18 @@
 #include<iostream>
 using namespace std;
 #include<string>
+#include<cmath>
 
 int hash_string(string s) {
     // implement hashing function here
     int m = 71;
+    int p = 31;
+    
     int sum=0;
     for(int i=0;i<s.length();i++){
 		char x = s[i];
 		int a = x;
-		sum = sum + a;
+		sum = sum + a*pow(p,i);
 	}
 	return sum%m;
 }
