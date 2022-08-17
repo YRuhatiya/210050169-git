654930f00623bdaa468995174bd7fb23507debfd
YRuhatiya <yashruhatiya2003@gmail.com>
2022-08-17 23:40:36 +0530

    Add hashing header and string read

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "hashing.hpp"
+#include "hashing.cpp"
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    cout << hash_string(argv[1]) << endl;
+}
